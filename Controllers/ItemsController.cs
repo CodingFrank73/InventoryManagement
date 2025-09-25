db2dabb375c146151ced1be8569e27a173580487
codingfrnk73 <codingfrank73@gmail.com>
2025-09-25 07:46:55 +0200

    Create Item hinzugefügt

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,10 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InventoryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryManagement.Controllers
 {
     public class ItemsController : Controller
     {
         public IActionResult Overview() 
+        {
+            Item itemOne = new Item 
+            {
+                Id = 1,
+                Name = "Fritzbox",
+                Description = "Einfacher Router",
+                Stock = 50
+            };
+
+            Item itemTwo = new Item
+            {
+                Id = 2,
+                Name = "Lampe XY",
+                Description = "Deckenleuchte",
+                Stock = 20
+            };
+
+            IEnumerable<Item> items = new List<Item> { itemOne, itemTwo };
+            return View(items);
+        }
+
+        public IActionResult Create() 
         {
             return View();
         }
